36241fee1eac4064f23fe1c905c63e7395d2673a
steven1618-01 <stu019022@gmail.com>
2020-06-14 22:39:26 -0700

    Initial Commit

---
+++ dayi 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dayi 1
+//
+//  Created by stu019022 on 2020/6/14.
+//  Copyright © 2020 stu019022. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
