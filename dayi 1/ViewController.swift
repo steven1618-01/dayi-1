e81157d0dac5b15c6ead970220335affae2cdd07
steven1618-01 <stu019022@gmail.com>
2020-06-14 22:48:21 -0700

    Self_work

--- dayi 1/ViewController.swift
+++ dayi 1/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var button1: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button_acteon(_ sender: Any) {
+        label1.text = "Hollow World"
+    }
+    
 }
 
